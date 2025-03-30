828308c330bcf762937ed26643aa621066d8d0a1
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2025-03-30 15:21:38 +0700

    hello

--- classroommanager/settings.py
+++ classroommanager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i^aguneu-oqe48ti!8j&b_aadox+^j76khf@mgu1m!$nb+sp0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
