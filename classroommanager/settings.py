e7a59da355a2d28b3589da7cd29dda69a2488b3c
Jesse Tong <tongnguyenminhkhang21012020@gmail.com>
2025-03-30 14:50:35 +0700

    test

--- classroommanager/settings.py
+++ classroommanager/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'https://classroom-manager.sgp.dom.my.id/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
